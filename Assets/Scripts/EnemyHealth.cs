fa48bbaf40ad349a7e4bd8dd80d24fbfb8117c87
saar-amsalem <saar2320@gmail.com>
2023-07-19 23:42:49 +0300

    new scene

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    NavMeshAgent navMeshAgent;
+
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        BroadcastMessage("OnDamageTaken");
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
+    }
+}
